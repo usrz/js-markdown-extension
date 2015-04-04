2226066ad154e0c0426a4655e353fade18e24abd
Pier Fumagalli <ianosh@gmail.com>
2015-04-04 21:55:40 +0900

    Tag versions.

--- pack.sh
+++ pack.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-zip -rp extension.zip css/* icons/* js/* manifest.json
+TAG=`node -e 'console.log(require("./manifest.json").version)'`
+
+echo "Tagging and packing release ${TAG}"
+git tag -m "Release ${TAG}" $TAG || exit 1
+zip -rp "extension-${TAG}.zip" css/* icons/* js/* manifest.json
